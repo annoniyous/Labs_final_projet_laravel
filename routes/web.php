53de4507c820b5f48154bd41a28db3991086f761
annoniyous <bouysantiman.y@outlook.com>
2021-03-29 16:22:29 +0200

    Prepartion de toute les migrations

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/services', function (){
+    return view('pages.services');
+});
+Route::get('/blog', function (){
+    return view('pages.blog');
+});
+Route::get('/contact', function (){
+    return view('pages.contact');
+});
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
